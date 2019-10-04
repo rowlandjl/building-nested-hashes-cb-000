87ed8ef77a3edbabe6faf1ef369b6d9aa272c5d7
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-04 20:13:57 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,7 +22,8 @@ def third_challenge
         :age => "50"
         },
       :matriarch => {
-        :name => "Lady Capulet"
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
         #your key/value pairs here
